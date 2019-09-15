a74e6a8677204c2a0393b62a3507b331e047ef52
LucasE2996 <moeda.60@gmail.com>
2019-09-15 03:48:30 -0300

    add loading

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,11 +6,14 @@ import Api from "../services/Api";
 import "./Login.css";
 import wintaylorLogo from "../assets/wintaylor_logo.svg";
 import FlashMessage from "./components/FlashMessage";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompass } from '@fortawesome/free-solid-svg-icons';
 
 function Login({ history }) {
   const [fleshMessage, setFleshMessage] = useState({ status: false });
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const id = localStorage.getItem("user");
@@ -21,11 +24,13 @@ function Login({ history }) {
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setLoading(true);
 
     const result = await Api.post("sessions", {
       email: userEmail,
       password: userPassword
     }).catch(error => {
+      setLoading(false);
       console.error(error);
     });
 
@@ -34,6 +39,7 @@ function Login({ history }) {
         message: 'email or password is wrong!',
         status: true
       });
+      setLoading(false);
       return;
     }
 
@@ -47,34 +53,48 @@ function Login({ history }) {
     } else {
       history.push(`/main/${user.id}`);
     }
+    setLoading(false);
   }
 
   return (
-    <div className="login-container">
-      <h2> Game Voting </h2> <img src={wintaylorLogo} alt="Wintaylor Logo" />
-      <form onSubmit={handleSubmit}>
-        <input
-          className="box"
-          type="email"
-          placeholder="Enter with your business email"
-          onChange={e => setUserEmail(e.target.value)}
-          required
-        />
-        <input
-          className="box"
-          type="password"
-          placeholder="Password"
-          onChange={e => setUserPassword(e.target.value)}
-          required
-        />
-        <button className="box" type="submit">
-          Login
-        </button>
-        <FlashMessage
-          data={fleshMessage}
-          closeHandler={() => setFleshMessage({ status: false })}
-        />
-      </form>
+    <div>
+      { loading
+        ? <div className="loading">
+            <FontAwesomeIcon
+              className="icon"
+              icon={faCompass}
+              spin
+            />
+          </div>
+        : (<div className="login-container">
+            <h2> Game Voting </h2>
+            <img src={wintaylorLogo} alt="Wintaylor Logo" />
+            <form onSubmit={handleSubmit}>
+              <input
+                className="box"
+                type="email"
+                placeholder="Enter with your business email"
+                onChange={e => setUserEmail(e.target.value)}
+                required
+              />
+              <input
+                className="box"
+                type="password"
+                placeholder="Password"
+                onChange={e => setUserPassword(e.target.value)}
+                required
+              />
+              <button className="box" type="submit">
+                Login
+              </button>
+            </form>
+            <FlashMessage
+                data={fleshMessage}
+                closeHandler={() => setFleshMessage({ status: false })}
+              />
+          </div>)}
+
+
     </div>
   );
 }
