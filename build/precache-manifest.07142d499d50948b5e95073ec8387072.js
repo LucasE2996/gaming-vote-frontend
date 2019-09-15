fa9a3ddaec1f0cb720a46bfbaf3a3279d6fc1da2
LucasE2996 <moeda.60@gmail.com>
2019-09-15 00:55:41 -0300

    build

---
+++ build/precache-manifest.07142d499d50948b5e95073ec8387072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86473c255b8733d1aa16bb2a278dcdaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f022a93ae57b88c4d66a",
+    "url": "/static/css/main.5cb5ae14.chunk.css"
+  },
+  {
+    "revision": "bc8daa33a590c56d0630",
+    "url": "/static/js/2.d92486f9.chunk.js"
+  },
+  {
+    "revision": "f022a93ae57b88c4d66a",
+    "url": "/static/js/main.cad4d3ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a036854f6ab657e3c1a8315df7f3c9b",
+    "url": "/static/media/Mario-Kart-DS.7a036854.ttf"
+  },
+  {
+    "revision": "ddd7100377eb4909c8af83f3128d01ef",
+    "url": "/static/media/wintaylor_logo.ddd71003.svg"
+  }
+]);
